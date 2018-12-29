3200f69e12f0f34996eb6242738b73736cff5221
Timag <rstudio@ip-172-31-32-6.eu-central-1.compute.internal>
2018-12-29 19:29:18 +0000

    update

--- R/addSnippet.R
+++ R/addSnippet.R
@@ -2,7 +2,7 @@ add <- function(snippetName, code, filePath = "~/.R/snippets/r.snippets"){
   if(!file.exists(filePath)) stop("r.snippets file not found. Check the r.snippets path. Might have to make a first edit
                                   to file via Tools-Global Options-Code-Edit Snippets to make it available")
 
-  data <- read.csv2(file = "~/.R/snippets/r.snippets",
+  data <- read.csv2(file = filePath,
                     header = FALSE, stringsAsFactors = FALSE)[, 1]
   idx <- grep("snippet ", data)
   takenNames <- sapply(strsplit(data[idx], "snippet "), "[", 2)
@@ -10,6 +10,8 @@ add <- function(snippetName, code, filePath = "~/.R/snippets/r.snippets"){
   if(snippetName %in% takenNames) stop("snippetName is already taken, please choose another one")
 
   snippetNameIn <- paste("snippet", snippetName)
+  code <- gsub("  ", "\t", code)
+  code <- gsub("\n", "\n\t", code)
   codeIn <- paste0("\t", code)
   data <- data.frame(
     text = paste(c(snippetNameIn, codeIn, as.character(data)), collapse = "\n"),
@@ -20,6 +22,17 @@ add <- function(snippetName, code, filePath = "~/.R/snippets/r.snippets"){
   file.edit(filePath)
 }
 
+SnippetName <- function(snippetName, filePath = "~/.R/snippets/r.snippets"){
+  data <- read.csv2(file = filePath,
+                    header = FALSE, stringsAsFactors = FALSE)[, 1]
+  idx <- grep("snippet ", data)
+  takenNames <- sapply(strsplit(data[idx], "snippet "), "[", 2)
+
+  while(snippetName %in% takenNames){
+    snippetName <- paste0(snippetName, substring(snippetName, 3, 3))
+  }
+  return(snippetName)
+}
 
 addSnippet <- function(){
   library(shiny)
@@ -27,7 +40,7 @@ addSnippet <- function(){
   library(rstudioapi)
 
   code <- getActiveDocumentContext()$selection[[1]]$text
-  name <- abbreviate(code, 5, named = FALSE)
+  name <- SnippetName(substring(code, 1, 3))
 
   ui <- miniPage(
     gadgetTitleBar("Add RStudio snippets"),
@@ -39,6 +52,7 @@ addSnippet <- function(){
   )
 
   server <- function(input, output, session) {
+
     observeEvent(input$done, {
       add(snippetName = input$snippetName,
           code = input$snippetCode,
@@ -46,10 +60,8 @@ addSnippet <- function(){
       stopApp()
     })
   }
+
   viewer <- dialogViewer("Edit snippet and press 'Done',...", width = 600,
                          height = 320)
   runGadget(ui, server, viewer = viewer)
 }
-
-
-
